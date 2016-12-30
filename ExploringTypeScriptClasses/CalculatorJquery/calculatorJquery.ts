2222602e9e48e3d69082cf3af6f1d6932b7bbe4b
GitHubStan <GitHubStan@gmail.com>
2016-12-30 11:49:13 -0800

    Add jQuery version of the calculator

---
+++ ExploringTypeScriptClasses/CalculatorJquery/calculatorJquery.ts
@@ -0,0 +1,45 @@
+﻿/// <reference path="jquery.d.ts" />
+
+$(document).ready(() => {
+    var calc = new CalculatorJquery('X', 'Y', 'Output');
+});
+
+class CalculatorJquery
+{
+    private x: JQuery;
+    private y: JQuery;
+    private output: JQuery;
+
+    constructor(xId: string, yId: string, outputId) {
+        this.x = $("#" + xId);
+        this.y = $("#" + yId);
+        this.output = $("#" + outputId);
+        this.wireEvents();
+    }
+
+    private wireEvents() {
+        $('#Add').click(
+            event => {
+                let x = parseInt(this.x.val());
+                let y = parseInt(this.y.val());
+                var result = this.add(x, y);
+                this.output.html(result.toString());
+            });
+        $('#Subtract').click(
+            event => {
+                let x = parseInt(this.x.val());
+                let y = parseInt(this.y.val());
+                var result = this.subtract(x, y);
+                this.output.html(result.toString());
+            }
+        );
+    };
+
+    add(x: number, y: number) {
+        return x + y;
+    }
+
+    subtract(x: number, y: number) {
+        return x - y;
+    }
+}
