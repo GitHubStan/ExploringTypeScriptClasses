1ba497c081c2ed27c9e0d272cf9897893ac04148
GitHubStan <GitHubStan@gmail.com>
2016-12-30 16:20:14 -0800

    Add base Auto that Car extends. Also added jquery library.

--- ExploringTypeScriptClasses/Car/car.ts
+++ ExploringTypeScriptClasses/Car/car.ts
@@ -1,12 +1,61 @@
 ﻿class Engine {
     constructor(public horsePower: number, public engineType: string) { }
+
+    start(callback: (startStatus: boolean, engineType: string) => void) {
+        window.setTimeout(() => {
+                callback(true, this.engineType);
+            }, 1000);
+    }
+
+    stop(callback: (stopStatus: boolean, engineType: string) => void) {
+        window.setTimeout(() => {
+                callback(false, this.engineType);
+            }, 1000);
+    }
 }
 
-class Car {
-    private _engine: Engine;
+class Accessory {
+    constructor(public accessoryNumber: number, public title: string) {}
+}
 
-    constructor(engine: Engine) {
-        this._engine = engine;
+class Auto {
+    private _basePrice: number;
+    private _engine: Engine;
+    make: string;
+    model: string;
+    accessoryList: string;
+    
+    constructor(basePrice: number, engine: Engine, make: string, model: string) {
+        this.basePrice = basePrice;
+        this.engine = engine;
+        this.make = make;
+        this.model = model;
+    }
+
+    calculateTotal(): number {
+        const taxRate = .08;
+        return this.basePrice + (taxRate * this.basePrice);
+    }
+
+    addAccessories(...accessories: Accessory[]) {
+        this.accessoryList = '';
+        for (let i = 0; i < accessories.length; i++) {
+            var accessory = accessories[i];
+            this.accessoryList += accessory.accessoryNumber + ' ' + accessory.title + '<br />';
+        }
+    }
+
+    getAccessoryList(): string {
+        return this.accessoryList;
+    }
+
+    get basePrice(): number {
+        return this._basePrice;
+    }
+
+    set basePrice(value: number) {
+        if (value <= 0) throw 'price must be >= 0';
+        this._basePrice = value;
     }
 
     get engine(): Engine {
@@ -17,8 +66,23 @@ class Car {
         if (value === undefined) throw 'Please supply an engine';
         this._engine = value;
     }
+}
 
-    start() : void {
-        alert(`Car engine started ${this._engine.engineType}`);
+class Car extends Auto {
+    bedLength: string;
+    fourByFour: boolean;
+
+    constructor(basePrice: number,
+        engine: Engine,
+        make: string,
+        model: string,
+        bedLength: string,
+        fourByFour:
+        boolean) {
+
+        super(basePrice, engine, make, model);
+
+        this.bedLength = bedLength;
+        this.fourByFour = fourByFour;
     }
 }
--- ExploringTypeScriptClasses/Car/onload.ts
+++ ExploringTypeScriptClasses/Car/onload.ts
@@ -1,11 +1,11 @@
-window.onload = function () {
+﻿window.onload = () => {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start();
-    var truck = new Car(42000, new Engine(269, 'V6'), 'Honda', 'Crosstour', 'Long bed', true);
+
+    var truck = new Car(42000, new Engine(402, 'V8'), 'Honda', 'Crosstour', 'Long bed', true);
     truck.addAccessories(new Accessory(1234, 'bed liner'), new Accessory(54322, 'sun roof'));
-    truck.engine.start(function (startStatus, engineType) {
+    truck.engine.start((startStatus: boolean, engineType: string) => {
         alert(engineType + ' was started!');
     });
 };
-//# sourceMappingURL=onload.js.map
