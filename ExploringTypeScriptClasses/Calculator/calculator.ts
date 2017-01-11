8c7269f714a580203fcd1a0b2592c2d7ccd3ef26
GitHubStan <GitHubStan@gmail.com>
2017-01-10 19:46:00 -0800

    Creating modules and adding toastr. Also added tsconfig.json .

--- ExploringTypeScriptClasses/Calculator/calculator.ts
+++ ExploringTypeScriptClasses/Calculator/calculator.ts
@@ -1,36 +1,47 @@
-﻿window.onload = function() {
-    var calc = new Calculator('X', 'Y', 'Output');
+﻿/// <reference path="../scripts/typings/toastr.d.ts" />
+
+//For build errors, I updated the proj file to not build TypeScript files. See the following links.
+//https://github.com/Microsoft/TypeScript/issues/2294
+//https://github.com/Microsoft/TypeScript/issues/7091
+
+window.onload = () => {
+    var calc = new NumberTools.Calculator('X', 'Y', 'Output');
 }
 
-class Calculator {
-    private x: HTMLInputElement;
-    private y: HTMLInputElement;
-    private output: HTMLSpanElement;
+namespace NumberTools {
 
-    constructor(xId: string, yId: string, outputId: string) {
-        this.x = <HTMLInputElement>document.getElementById(xId);
-        this.y = <HTMLInputElement>document.getElementById(yId);
-        this.output = <HTMLSpanElement>document.getElementById(outputId);
-        this.wireEvents();
-    }
+    export class Calculator {
+        private x: HTMLInputElement;
+        private y: HTMLInputElement;
+        private output: HTMLSpanElement;
 
-    wireEvents() {
-        document.getElementById('Add').addEventListener('click',
-            event => {
-                this.output.innerHTML = this.add(parseInt(this.x.value), parseInt(this.y.value)).toString();
-            });
+        constructor(xId: string, yId: string, outputId: string) {
+            this.x = <HTMLInputElement>document.getElementById(xId);
+            this.y = <HTMLInputElement>document.getElementById(yId);
+            this.output = <HTMLSpanElement>document.getElementById(outputId);
+            this.wireEvents();
+        }
 
-        document.getElementById('Subtract').addEventListener('click',
-            event => {
-                this.output.innerHTML = this.subtract(parseInt(this.x.value), parseInt(this.y.value)).toString();
-            });
-    }
+        wireEvents() {
+            document.getElementById('Add').addEventListener('click',
+                event => {
+                    this.output.innerHTML = this.add(parseInt(this.x.value), parseInt(this.y.value)).toString();
+                });
 
-    add(x: number, y: number) {
-        return x + y;
-    }
+            document.getElementById('Subtract').addEventListener('click',
+                event => {
+                    this.output.innerHTML = this.subtract(parseInt(this.x.value), parseInt(this.y.value)).toString();
+                });
+        }
 
-    subtract(x: number, y: number) {
-        return x - y;
+        add(x: number, y: number) {
+            toastr.info("Adding 2 numbers...");
+            return x + y;
+        }
+
+        subtract(x: number, y: number) {
+
+            return x - y;
+        }
     }
 }
