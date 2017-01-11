27656257a5fe82f7ad293a3789d4af31419660d0
GitHubStan <GitHubStan@gmail.com>
2017-01-10 20:16:39 -0800

    Add ICalculator interface and public toast method.

--- ExploringTypeScriptClasses/Calculator/calculator.ts
+++ ExploringTypeScriptClasses/Calculator/calculator.ts
@@ -5,12 +5,21 @@
 //https://github.com/Microsoft/TypeScript/issues/7091
 
 window.onload = () => {
-    var calc = new NumberTools.Calculator('X', 'Y', 'Output');
+    var calc: ICalculator = new NumberTools.Calculator('X', 'Y', 'Output');
+    calc.toast("Calculator is ready to use.");
+}
+
+
+export interface ICalculator {
+    add(x: number, y: number);
+    subtract(x: number, y: number);
+    toast(message: string);
 }
 
 namespace NumberTools {
 
-    export class Calculator {
+    export class Calculator implements ICalculator {
+
         private x: HTMLInputElement;
         private y: HTMLInputElement;
         private output: HTMLSpanElement;
@@ -35,13 +44,15 @@ namespace NumberTools {
         }
 
         add(x: number, y: number) {
-            toastr.info("Adding 2 numbers...");
             return x + y;
         }
 
         subtract(x: number, y: number) {
-
             return x - y;
+        }
+
+        toast(message: string) {
+            toastr.info(message);
         }
     }
 }
